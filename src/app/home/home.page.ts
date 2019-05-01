efe58d2c4b24400290cf2755a7e7429a9702127b
マヌエル <fmartinezmanuel@gmail.com>
2019-05-01 16:13:20 -0500

    Visual adjustments for improve UX / UI

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { FormControl, FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 import { NavController } from '@ionic/angular';
-import { AlertController } from '@ionic/angular';
+import { AlertController, LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -10,10 +10,11 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage{
-  loginForm    : FormGroup;
-  registerForm : FormGroup;
-  loginView    : boolean = true;
-  registerView : boolean = false;
+  loginForm     : FormGroup;
+  registerForm  : FormGroup;
+  loginView     : boolean = true;
+  registerView  : boolean = false;
+  loadingPresent: boolean = false;
   email;
   password;
   firstName;
@@ -22,11 +23,13 @@ export class HomePage{
   passwordVerify;
   emailRegister;
 
+
   constructor(
-    private authService   : AuthService,
-    private formBuilder   : FormBuilder,
-    private navCtrl       : NavController,
-    public alertController: AlertController,
+    private authService     : AuthService,
+    private formBuilder     : FormBuilder,
+    private navCtrl         : NavController,
+    public alertController  : AlertController,
+    public loadingController: LoadingController
   ){
     this.loginForm = formBuilder.group({
       email   :['', Validators.compose([
@@ -61,22 +64,44 @@ export class HomePage{
     this.passwordVerify   = this.registerForm.controls['passwordVerify'];
   }//constructor ends..
 
+  async ngOnInit(){
+    await this.verifyAuth();
+  }
+
+  async verifyAuth(){
+    let response = await this.authService.getUserToken();
+    console.log('verifyAuth: ');
+    console.log(response);
+
+    if(response != false){
+      this.navCtrl.navigateForward(`/catalogs`);
+    }
+  }
+
   async registerNewUser(){
     this.registerForm.setErrors( { MatchPassword: true } );
     console.log('Registrando nuevo usuario!');
+    this.timeForLoading();
+    this.presentLoading();
     await this.authService.register({
       "email"    : this.emailRegister.value,
       "firstname": this.firstName,
       "lastname" : this.lastName,
       "password" : this.passwordRegister.value
     });
-    this.presentAlert('', 'Registro satisfactorio', 'Por seguridad debes iniciar sesión.');
+    this.timeForLoading();
+    this.loadingController.dismiss();
+    this.presentAlert('', 'Registro, ¡Satisfactorio!', 'Por seguridad debe iniciar sesión.');
     this.switchRegisterLogin();
   }//registerNewUser ends..
 
   async sendLogin(){
+    this.timeForLoading();
+    this.presentLoading();
     this.loginForm.setErrors( { MatchPassword: true } );
     let response = await this.authService.login( this.email.value, this.password.value );
+    this.timeForLoading();
+    this.loadingController.dismiss();
     console.log('Resultado de login: '+response);
     if(response == true){
       this.navCtrl.navigateForward(`/catalogs`);
@@ -87,6 +112,12 @@ export class HomePage{
     this.loginView    = !this.loginView;
     this.registerView = !this.registerView;
   }//switchRegisterLogin ends..
+
+  timeForLoading(){
+    console.log('Inicio: '+this.loadingPresent);
+    this.loadingPresent = !this.loadingPresent;
+    console.log('Fin: '+this.loadingPresent);
+  }
 
   private MatchPassword(AC: AbstractControl) {
      const newPassword     = AC.get('passwordRegister').value
@@ -109,6 +140,18 @@ export class HomePage{
     });
 
     await alert.present();
-  }
+  }//presentAlert ends..
+
+  async presentLoading() {
+      const loading = await this.loadingController.create({
+        message: 'Un momento por favor..',
+        duration: 2000
+      });
+      await loading.present();
+
+      const { role, data } = await loading.onDidDismiss();
+
+      console.log('Loading dismissed!');
+    }
 
 }//HomePage ends..
